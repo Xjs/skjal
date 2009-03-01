cc1c2e6e6ccdf5377414757ed5443abac11a6350
Jannis Andrija Schnitzer <Xjs@itisme.org>
2009-03-01 17:44:01 +0100

    initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Skjal
+//
+//  Created by Jannis Schnitzer on 3/1/09.
+//  Copyright It Is Me Org. 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
